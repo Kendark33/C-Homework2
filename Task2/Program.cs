4e30624b3ec227f8fd5365ec82d1223697bea198
Kendark33 <you@example.com>
2022-10-09 20:50:50 +0300

    выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+
+void AnyThing()
+{
+    int newNumber = (new Random()).Next(1, 10000);
+    Console.WriteLine($"Base number: {newNumber}");
+    string numericAsString = newNumber.ToString();
+        if (newNumber > 99)
+    {
+        int result = int.Parse(numericAsString[2].ToString());
+        Console.WriteLine($" Третье цифра заданного числа: {result}");
+        return;
+    }
+    else
+        Console.WriteLine($"Третьей цифры числа нет");
+}
+AnyThing();
