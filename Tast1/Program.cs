9ac1bcd86cce1154cb6d3b13ffcbdc22ac8594ba
Kendark33 <you@example.com>
2022-10-09 20:48:40 +0300

    принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.

---
+++ Tast1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+void Task()
+{
+    Console.WriteLine("Введите трехзначное число");
+    string newNumber= Console.ReadLine();
+    string numericAsString = newNumber.ToString();
+    int result = int.Parse (numericAsString[1].ToString());
+    Console.WriteLine($"SecondNumeric: {result}");
+}
+Task();
